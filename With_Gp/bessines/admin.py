4d98bafb6d2fc2a72de9bb80bf6c308583041ecf
LitvinT <litvin.lara74mail.ru@gmail.com>
2023-01-19 16:45:32 +0300

    2

--- With_Gp/bessines/admin.py
+++ With_Gp/bessines/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_published')
+    search_fields = ('name', )
+    list_filter = ('is_published', )
+    ordering = ('is_published', 'name')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'is_published')
+    list_filter = ('category', 'is_published')
+    search_fields = ('name', )
+    ordering = ('is_published', 'name')
--- With_Gp/bessines/models.py
+++ With_Gp/bessines/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=64,
+        verbose_name='название'
+    )
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='публикация'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'main_categories'
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=128,
+        verbose_name='название'
+    )
+    descr = models.CharField(
+        max_length=2048,
+        verbose_name='описание'
+    )
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='публикация'
+    )
+    image = models.ImageField(
+        upload_to='products/',
+        verbose_name='картинка'
+    )
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+        verbose_name='категория'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'main_products'
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
